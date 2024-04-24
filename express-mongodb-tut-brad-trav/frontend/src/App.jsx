3f8a734a26e48bfc9213daf75782d7b3a56f7c71
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-24 19:30:14 +0800

    commit of the day

--- express-mongodb-tut-brad-trav/frontend/src/App.jsx
+++ express-mongodb-tut-brad-trav/frontend/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
       <Router>
         <div className="container">
           <Header />
+          {/* Router */}
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
